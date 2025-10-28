3a96b16a41d2ffa4c8140cfab01a0ef870c582a7
Virtual Page <virtualpage.dev@gmail.com>
2025-10-28 00:01:07 -0300

    Update trintemo.controller.ts

--- src/modules/trintemo.controller.ts
+++ src/modules/trintemo.controller.ts
@@ -1,7 +1,31 @@
 import type { Request, Response } from 'express';
 import { handleControllerError } from '../utils/errors';
-import { confirmationService, drinkService, messageService, notConfirmationService, notDrinkService } from './trintemo.service';
+import { confirmationService, drinkService, guestsService, messageService, newGuestsService, notConfirmationService, notDrinkService, stepService } from './trintemo.service';
 
+export const guests = async (req: Request, res: Response): Promise<void> => {
+
+    try {
+        const guests = await guestsService();
+        res.status(200).json(guests);
+        return;
+
+    } catch (error: any) {
+        handleControllerError(res, error);
+    }
+}
+
+export const newGuest = async (req: Request, res: Response): Promise<void> => {
+    const convidado = req.params.nome;
+
+    try {
+        await newGuestsService(convidado);
+        res.status(204).end();
+        return;
+
+    } catch (error: any) {
+        handleControllerError(res, error);
+    }
+}
 export const confirmation = async (req: Request, res: Response): Promise<void> => {
     const convidado = req.params.nome;
 
@@ -67,3 +91,17 @@ export const message = async (req: Request, res: Response): Promise<void> => {
         handleControllerError(res, error);
     }
 };
+
+export const step = async (req: Request, res: Response): Promise<void> => {
+    const convidado = req.params.nome;
+    const step = Number(req.params.step);
+
+    try {
+        await stepService(convidado, step);
+        res.status(204).end();
+        return;
+
+    } catch (error: any) {
+        handleControllerError(res, error);
+    }
+}
